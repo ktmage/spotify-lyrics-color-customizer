49b3f76ccc29f36b14442f0c4540da13cff2d905
ktmage <yasashisainternet@gmail.com>
2024-07-22 03:50:06 +0900

    [add] : localization.jsを追加
    動的にテキストを変更するためのファイルを追加しました。

---
+++ src/popup/localization.js
@@ -0,0 +1,7 @@
+document.getElementById('popup-title').textContent = chrome.i18n.getMessage('popup_title');
+document.getElementById('save').textContent = chrome.i18n.getMessage('save');
+document.getElementById('reset').textContent = chrome.i18n.getMessage('reset');
+document.getElementById('background-color-label').textContent = chrome.i18n.getMessage('background_color_label');
+document.getElementById('active-color-label').textContent = chrome.i18n.getMessage('active_color_label');
+document.getElementById('inactive-color-label').textContent = chrome.i18n.getMessage('inactive_color_label');
+document.getElementById('passed-color-label').textContent = chrome.i18n.getMessage('passed_color_label');
